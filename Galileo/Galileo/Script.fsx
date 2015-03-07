2426deb4ea5aff9469fe69a6176a131fd7203f43
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2015-03-07 13:35:43 -0800

    Draw one red triangle on window

---
+++ Galileo/Galileo/Script.fsx
@@ -0,0 +1,19 @@
+﻿#r @"bin\Debug\Galileo.dll"
+
+open Planets
+open System
+
+Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+
+let app = Planets.App.init () 
+
+Galileo.init ()
+
+let shaderProgram = Planets.App.loadShaders ()
+Planets.App.clear ()
+Planets.App.draw app
+
+Planets.App.drawColors (shaderProgram)
+Galileo.fooTriangle (-1.0f, -1.0f, 0.0f) ( 1.0f, -1.0f, 0.0f) ( 0.0f,  1.0f, 0.0f)
+
+App.draw app
